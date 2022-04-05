4a7f41a93677546d3ba56586698cec6372ff1318
justcoddev <37991332+justcoddev@users.noreply.github.com>
2022-04-05 16:26:29 -0500

    Rxjs Observable

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -23,13 +23,19 @@ export class MediaPlayerComponent implements OnInit, OnDestroy {
   constructor(private multimediaService: MultimediaService) {}
 
   ngOnInit(): void {
-    const observer1$: Subscription = this.multimediaService.callback.subscribe(
-      (Response: TrackModel) => {
-        console.log('Recibiendo cancion....', Response);
-      }
-    );
 
-    this.listObservers$ = [observer1$];
+
+    const observable1$ = this.multimediaService.myObservable1$
+    .subscribe(
+      (responseOk)=>{
+        // TODO: next()
+        console.log('El agua va perfecta!',responseOk)
+      },
+      (responseFail)=>{
+        // TODO: error()
+        console.log('Se tapo la tuberia',responseFail)
+      },
+    )
   }
   ngOnDestroy(): void {
     this.listObservers$.forEach(u => u.unsubscribe());
--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,9 +1,28 @@
+import { Observable, Observer } from 'rxjs';
 import { EventEmitter, Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MultimediaService {
-  callback: EventEmitter<any> = new EventEmitter<any>()
-  constructor() { }
+  callback: EventEmitter<any> = new EventEmitter<any>();
+
+  myObservable1$: Observable<any> = new Observable();
+  constructor() {
+    this.myObservable1$ = new Observable((observer: Observer<any>) => {
+      observer.next('💦');
+
+      setTimeout(() => {
+        observer.complete();
+      }, 1000);
+      setTimeout(() => {
+        observer.next('💦');
+      }, 2500);
+      setTimeout(() => {
+        observer.error('💦');
+      }, 3500);
+    });
+  }
+
+  private listenAllEventes(): void {}
 }
